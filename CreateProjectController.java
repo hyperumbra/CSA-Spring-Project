6368e3a8a65d5c9fe38410a9524b0317e311465b
Joshua Donegal <32005113+hyperumbra@users.noreply.github.com>
2018-04-07 15:03:10 -0400

    Update CreateProjectController.java

--- CreateProjectController.java
+++ CreateProjectController.java
@@ -1,10 +1,13 @@
 package application;
 
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
 import javafx.fxml.FXML;
-//import javafx.scene.paint.Color;
+import javafx.fxml.FXMLLoader;
 
 public class CreateProjectController {
 
@@ -59,6 +62,20 @@ public class CreateProjectController {
 	//Initializing the GUI
 	@FXML
 	private void initialize() {
+		
+		//Sets the initial text of the Labels that may be changed
+		projectNameLabel.setText("Project Name");
+		manufacturerLabel.setText("Manufacturer");
+		modelLabel.setText("Label");
+		
+		//Sets the initial color for the warning
+		warningLabel.setTextFill(javafx.scene.paint.Color.RED);
+		
+		//Sets the initial colors of the Labels of the Required Fields
+		projectNameLabel.setTextFill(javafx.scene.paint.Color.BLACK);
+		manufacturerLabel.setTextFill(javafx.scene.paint.Color.BLACK);
+		modelLabel.setTextFill(javafx.scene.paint.Color.BLACK);
+		
 		//Sets a warning for the user to be invisible until necessary
 		warningLabel.setVisible(false);
 		dismissButton.setVisible(false);
@@ -104,6 +121,13 @@ public class CreateProjectController {
 	private void dismissWarning() {
 		warningLabel.setVisible(false);
 		dismissButton.setVisible(false);
+	}
+	
+	@FXML
+	private void backToSelection() throws IOException {
+		Stage stage = (Stage)backButton.getScene().getWindow();
+		Scene selectProjectScene = new Scene(FXMLLoader.load(getClass().getResource("/CreateProject.fxml")));
+		stage.setScene(selectProjectScene);
 	}
 	
 }
